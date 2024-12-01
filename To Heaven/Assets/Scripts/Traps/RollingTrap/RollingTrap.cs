170ef0c67d10b6918a8648a9b3f925f127bc7fa4
DESKTOP-DV5N4BC\Anh Vu <anhvu019vta@gmail.com>
2024-12-01 18:05:22 +0700

    commit

---
+++ To Heaven/Assets/Scripts/Traps/RollingTrap/RollingTrap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingTrap : MonoBehaviour
+{
+    public float rotationSpeed;     // Tốc độ xoay
+    public float moveSpeed;         // Tốc độ di chuyển
+    public Vector3 moveDirection;   // Hướng di chuyển
+
+    void Update()
+    {
+        // Xoay bẫy
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+        // Di chuyển bẫy
+        transform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
