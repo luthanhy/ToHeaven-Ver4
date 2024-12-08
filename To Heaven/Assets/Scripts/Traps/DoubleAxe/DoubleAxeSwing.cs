4a97ba470ed1d84924b85fbeb0365770e1cbbcc6
DESKTOP-DV5N4BC\Anh Vu <anhvu019vta@gmail.com>
2024-12-08 16:03:30 +0700

    done DoubleAxeTraps

---
+++ To Heaven/Assets/Scripts/Traps/DoubleAxe/DoubleAxeSwing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleAxeSwing : MonoBehaviour
+{
+    // Đối tượng trung tâm để xoay quanh
+    public Transform centerObject;
+
+    // Tốc độ xoay
+    public float rotationSpeed = 30f;
+
+    void Update()
+    {
+        if (centerObject != null)
+        {
+            // Xoay đối tượng này quanh trục của centerObject
+            transform.RotateAround(centerObject.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
